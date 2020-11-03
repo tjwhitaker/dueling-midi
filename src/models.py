0b9872b12d897e67239b6d99557c3432960f6f00
Tim Whitaker <camp4climber@gmail.com>
2020-11-02 20:40:27 -0800

    Implement cookie cutter template structure

--- src/models.py
+++ src/models.py
@@ -11,8 +11,9 @@ class NoteLSTM(nn.Module):
         self.output_size = output_size
         self.hidden_size = hidden_size
         self.hidden_layers = hidden_layers
+        self.embedding_dimensions = 32
 
-        self.embedding = nn.Embedding(input_size, output_size)
+        self.embedding = nn.Embedding(input_size, self.embedding_dimensions)
         self.lstm = nn.LSTM(input_size, hidden_size,
                             hidden_layers, batch_first=True)
         self.decoder = nn.Linear(hidden_size, output_size)
@@ -31,31 +32,30 @@ class NoteLSTM(nn.Module):
 
 class NoteCNN(nn.Module):
     def __init__(self, input_size=128, output_size=128, sequence_length=64):
-        in_channels = 1
+        super(NoteCNN, self).__init__()
+
+        in_channels = 64
         self.input_size = input_size
         self.output_size = output_size
         self.sequence_length = sequence_length
+        self.embedding_dimensions = 32
 
-        self.embedding = nn.Embedding(input_size, output_size)
+        self.embedding = nn.Embedding(input_size, self.embedding_dimensions)
 
-        self.conv_layers = nn.Sequential([
+        self.conv_block = nn.Sequential(
             nn.Conv1d(in_channels, 16, kernel_size=8),
-            nn.MaxPool1d(3, stride=2, padding=1),
-            nn.ReLU(),
-            nn.Conv1d(16, 32, kernel_size=4),
-            nn.MaxPool1d(3, stride=2, padding=1),
             nn.ReLU()
-        ])
+        )
 
-        self.decoder = nn.Sequential([
-            nn.Linear(32*3*3, 256),
+        self.decoder = nn.Sequential(
+            nn.Linear(25, 256),
             nn.ReLU(),
             nn.Linear(256, output_size)
-        ])
+        )
 
     def forward(self, x):
         embedding = self.embedding(x)
-        features = self.conv_layers(embedding)
+        features = self.conv_block(embedding)
         logits = self.decoder(features)
 
         return logits
--- src/train.py
+++ src/train.py
@@ -1,14 +1,18 @@
 import numpy as np
 import torch
-from models.notelstm import NoteLSTM
+from models.notelstm import NoteLSTM, NoteCNN
 import utils
 
-epochs = 100
+epochs = 1
 sequence_length = 64
 batch_size = 64
 num_pitches = 128
 hidden_size = 256
 hidden_layers = 2
+
+#####################
+# LSTM Training Code
+#####################
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -50,3 +54,43 @@ for i in range(epochs):
 
 # Save model
 torch.save(model.state_dict(), "models/checkpoints/notelstm.model")
+
+
+#####################
+# CNN Training Code
+#####################
+
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# model = NoteCNN().to(device)
+
+# training_set = utils.get_training_set(sequence_length)
+# loader = torch.utils.data.DataLoader(training_set, batch_size=batch_size)
+
+# criterion = torch.nn.CrossEntropyLoss()
+# optimizer = torch.optim.Adam(model.parameters())
+
+# for i in range(epochs):
+#     print(f"EPOCH {i}")
+#     print("------------------------")
+
+#     total_loss = 0
+
+#     for i, (inputs, targets) in enumerate(loader):
+#         # (batch_size, sequence_length)
+#         inputs = inputs.to(device)
+#         targets = targets.to(device)
+
+#         output = model(inputs, hidden_state)
+
+#         loss = criterion(output, targets)
+#         total_loss += loss.item()
+
+#         model.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+
+#     print(f"Loss: {total_loss}\n")
+
+# # Save model
+# torch.save(model.state_dict(), "models/checkpoints/notelstm.model")
