6c18ae96ce80e08bdd406e498ff68fd53e3fbdd7
Tim Whitaker <camp4climber@gmail.com>
2020-11-08 22:17:51 -0800

    Improve cnn eval

--- src/predict_cnn.py
+++ src/predict_cnn.py
@@ -2,33 +2,39 @@ import torch
 from models import NoteCNN
 import utils
 
-sequence_length = 64
-num_pitches = 128
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def predict_cnn(input_sequence, device, sequence_length=64):
+    # Load model
+    model = NoteCNN().to(device)
 
-# Load model
-model = NoteCNN().to(device)
+    model.load_state_dict(torch.load("../models/notecnn.model"))
+    model.eval()
 
-model.load_state_dict(torch.load("../models/notecnn.model"))
-model.eval()
+    melody = []
 
-training_set = utils.get_training_set(sequence_length)
-input_sequence = torch.tensor(training_set[0][0]).unsqueeze(0).to(device)
-melody = []
+    # Generate sequence
+    for _ in range(sequence_length):
+        output = model(input_sequence)
 
-# Generate sequence
-for _ in range(sequence_length):
-    output = model(input_sequence)
+        output = torch.functional.F.softmax(torch.squeeze(output), dim=0)
+        note = torch.distributions.Categorical(output).sample()
 
-    output = torch.functional.F.softmax(torch.squeeze(output), dim=0)
-    note = torch.distributions.Categorical(output).sample()
+        # Shift sequence and replace oldest note with prediction
+        input_sequence = torch.roll(input_sequence, shifts=-1, dims=1)
+        input_sequence[0][-1] = note.item()
 
-    # Shift sequence and replace oldest note with prediction
-    input_sequence = torch.roll(input_sequence, shifts=-1, dims=1)
-    input_sequence[0][-1] = note.item()
+        melody.append(note.item())
 
-    melody.append(note.item())
+    return melody
 
-print(training_set[0][0])
-print(melody)
+
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    input_sequence = torch.tensor([[61, 61,  0, 63,  0, 66,  0,  0, 68,  0,  0, 70,  0,  0,  0,  0, 66, 66, 66,  0,  0, 63, 63, 63,
+                                    63,  0,  0, 68, 68, 68, 68, 68, 68, 68, 68, 68, 68, 68, 68, 68, 68, 68, 68,  0,  0,  0,  0,  0,
+                                    0,  0,  0,  0,  0, 68, 68, 68, 68,  0,  0, 65, 65, 65,  0,  0]]).to(device)
+
+    melody = predict_cnn(input_sequence, device)
+
+    print(melody)
